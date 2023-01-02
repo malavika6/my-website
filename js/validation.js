5ac6ac7639281f0daed617f22e7f8352eb42a159
malavika6 <malavikavinod681@gmail.com>
2023-01-02 23:55:13 +0530

    first commit

---
+++ js/validation.js
@@ -0,0 +1,30 @@
+function formvalidation(){
+    let btn;
+
+    var name = document.getElementById('Name').value.trim();
+    var Email = document.getElementById('Email').value.trim();
+    var phone = document.getElementById('Phone').value.trim();
+    var msg = document.getElementById('Message').value.trim();
+
+
+    if(name ==="" || Email ==="" || phone ==="" || msg ==="")
+    {
+        btn=false;
+        
+        alert('Please fill all fields');
+        
+        
+    }else if(!/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+).([a-z]+).([a-z]+)?$/.test(Email)){
+        alert('Please enter a valid Email adress');
+
+    
+        btn=flase;
+    }
+    else if(!/^\d+$/.test(phone)){
+        alert('Please enter a valid Phone number');
+
+        btn=false;
+    }
+    
+    return btn;
+}
